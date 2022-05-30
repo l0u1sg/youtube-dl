9b47f341f1db56c51520b561733bb6d29065d62e
Louis Gallet <louis.gallet@tumo.world>
2022-05-30 17:12:19 +0200

    feat: :construction:  Starting interface and importing ytdl

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,20 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-
+  var youtubeToDownload
+  console.log(youtubeToDownload)
   return (
     <div className="App">
       <header className="App-header">
         <h1>YouTube Downloader</h1>
+        <p>Paste your YouTube video URL here 👇</p>
+        <input type='url' id='url'required placeholder='https://youtube.com/AZERTY'></input>
+        <button onClick={() => {
+          youtubeToDownload = document.getElementById('url') as HTMLInputElement
+          console.log(youtubeToDownload.value)
+          ytdl(youtubeToDownload.value).pipe(fs.createWriteStream('video.mp4'))
+        } }>Download</button>
+          
       </header>
     </div>
   )
