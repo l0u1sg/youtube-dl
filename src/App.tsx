ae961d9a8ac05d25d3181571f12ad323dafa9cab
Louis Gallet <louis.gallet@tumo.world>
2022-05-30 16:41:10 +0200

    feat: :sparkles:  Starting website with vite

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>hello world!</h1>
+      </header>
+    </div>
+  )
+}
+
+export default App
